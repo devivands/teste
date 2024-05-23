e8b84c9ebe68358ddaf65cbdba88205f03fadd81
Vinicius Ivan <chepsouza6@gmail.com>
2024-05-23 00:59:24 -0300

    primeirocommit

---
+++ Raiz/Scripts/index.js
@@ -0,0 +1,17 @@
+// Adiciona um evento de clique ao botão
+document.getElementById('meuBotao').addEventListener('click', function() {
+    // Redireciona para a página jogar
+    window.location.href = 'pagina-jogar.html';
+});
+
+// Adiciona um evento de clique ao botão
+document.getElementById('meuBotao2').addEventListener('click', function() {
+    // Redireciona para a página pokepedia
+    window.location.href = 'pagina-pokepedia.html';
+});
+
+// Adiciona um evento de clique ao botão
+document.getElementById('meuBotao3').addEventListener('click', function() {
+    // Redireciona para a página creditos
+    window.location.href = 'pagina-creditos.html';
+});
